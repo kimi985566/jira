50bbf5160e98654fdedc6eddadbc24fc38c9e4cf
Chengyu Yang <chengyu_yang@outlook.com>
2022-10-15 22:38:28 +0800

    update: useArray

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -39,3 +39,23 @@ export const useDebounce = <T>(value: T, delay?: number) => {
 
   return debounceValue;
 };
+
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
+
+  return {
+    value,
+    setValue,
+    add: (item: T) => {
+      setValue([...value, item]);
+    },
+    clear: () => {
+      setValue([]);
+    },
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.slice(index, 1);
+      setValue(copy);
+    },
+  };
+};
