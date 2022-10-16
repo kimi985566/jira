60d48e315da3eacf1d914a625e0d10006438b7b3
Chengyu Yang <chengyu_yang@outlook.com>
2022-10-16 10:03:16 +0800

    feat: json-server

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { ProjectListScreen } from "screen/project-list";
+import { LoginScreen } from "screen/login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      <LoginScreen />
+      {/* <ProjectListScreen /> */}
     </div>
   );
 }
